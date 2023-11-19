c35bb123fa0d125a5b0f6315c6f6ad91477b2d9c
Patryk 'pcpl2' Ławicki <kontakt@patryk-lawicki.pl>
2023-11-19 19:25:23 +0100

    Added build packages.

--- resources/custom.sh
+++ resources/custom.sh
@@ -3,3 +3,10 @@
 set -exu
 
 # Add your additional provisioning here for custom VM images.
+
+install_build_packages() {
+  ASSUME_ALWAYS_YES=yes pkg install gmake llvm-devel gdb lang/gcc devel/premake5
+}
+
+
+install_build_packages
